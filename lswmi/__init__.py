264a13036b7e71c2510d8b40e7b68ed03eb45f60
Armin Wolf <W_Armin@gmx.de>
2024-03-03 19:31:42 +0100

    Bump version to 1.2.0
    Bump utility version to 1.2.0.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- lswmi/__init__.py
+++ lswmi/__init__.py
@@ -2,7 +2,7 @@
 
 """Utility to retrieve information about WMI devices on Linux """
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Armin Wolf"
 __email__ = "W_Armin@gmx.de"
 __all__ = (
